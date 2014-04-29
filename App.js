e846b06704d61f893d0ed1cac6f28debfd76a24e
Barry Mullan <mullan@gmail.com>
2014-04-29 14:22:23 -0400

    first version

---
+++ App.js
@@ -0,0 +1,13 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc2/doc/">App SDK 2.0rc2 Docs</a>'},
+    launch: function() {
+        //Write app code here
+
+        console.log("launched");
+
+        console.log("second launched ");
+
+    }
+});
